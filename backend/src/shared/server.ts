a098b90b28a8d02b89ecd92995460c26ebfdce4d
guilhermeaw <guilherme.w@universo.univates.br>
2022-06-27 22:38:31 -0300

    ci: compose up detached

--- backend/src/shared/server.ts
+++ backend/src/shared/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
 app.listen(process.env.API_PORT, () => {
-  console.log(`Server up on port ${process.env.API_PORT}`);
+  console.log(`Server up on port ${process.env.API_PORT}!`);
 });
